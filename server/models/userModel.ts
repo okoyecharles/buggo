80b2cacf4c2861d574cbdf66775d493716c6f556
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-01-03 21:55:44 +0100

    Modified model

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -1,14 +1,18 @@
-import mongoose, { InferSchemaType } from "mongoose";
+import mongoose, { InferSchemaType } from 'mongoose';
 
-const UserSchema = new mongoose.Schema({
-  name: { type: String, required: true },
-  image: { type: String, required: true },
-  email: { type: String, unique: true, required: true },
-  password: { type: String, required: true },
-  admin: { type: Boolean, default: false }
-}, {
-  timestamps: true
-});
+const UserSchema = new mongoose.Schema(
+  {
+    name: { type: String, required: true },
+    image: { type: String, required: true },
+    email: { type: String, unique: true, required: true },
+    password: { type: String, required: true },
+    googleId: { type: String, required: false },
+    admin: { type: Boolean, default: false },
+  },
+  {
+    timestamps: true,
+  }
+);
 
 export type UserType = InferSchemaType<typeof UserSchema>;
 export default mongoose.model('User', UserSchema);
