43cc843588c57c60e4b0d8d0a724a60586f6ceb9
Charles <okoyecharles509@gmail.com>
2023-01-03 19:46:48 +0100

    Modify authentication middleware

--- server/middleware/authMiddleware.ts
+++ server/middleware/authMiddleware.ts
@@ -3,22 +3,26 @@ import AuthorizedRequest from '../types/request';
 import jwt from 'jsonwebtoken';
 
 const secret = process.env.JWT_SECRET || '';
-const protect = (req: AuthorizedRequest, res: Response, next: NextFunction) => {
+const protect = (req: AuthorizedRequest<any>, res: Response, next: NextFunction) => {
   const requestToken = req.headers.authorization;
+  let token = '';
 
   if (requestToken && requestToken.startsWith('Bearer')) {
-    const token = requestToken.split(' ')[1];
-    jwt.verify(token, secret, (err, user: any) => {
-      if (err) {
-        res.status(401).json({ message: 'Not authorized, token failed' });
-      } else {
-        req.user = user?.id;
-        next();
-      }
-    });
+    try {
+      token = requestToken.split(' ')[1];
+
+      const decoded: any = jwt.verify(token, secret);
+      req.user = decoded.id;
+
+      next();
+    } catch (err) {
+      return res.status(401).json({ message: 'Not authorized' });
+    }
   }
 
-  res.status(401).json({ message: 'Not authorized, no token' });
+  if (!token) {
+    res.status(400).json({ message: 'Not authorized, no token' });
+  }
 };
 
 export default protect;
--- server/types/request.ts
+++ server/types/request.ts
@@ -1,8 +1,8 @@
 import { Request } from "express";
 import jwt from "jsonwebtoken";
 
-interface AuthorizedRequest extends Request {
-  user: string | jwt.JwtPayload;
+type AuthorizedRequest<T> = Request<never, never, T> & {
+  user?: string | jwt.JwtPayload;
 };
 
 export default AuthorizedRequest;
