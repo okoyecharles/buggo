61ee37adb3048249852a30cf7327e43525a542b4
Charles <okoyecharles509@gmail.com>
2023-01-03 14:47:45 +0100

    Configure node server and connect to mongoDB

--- server/index.ts
+++ server/index.ts
@@ -1,1 +1,31 @@
-console.log('Hey');
+import dotenv from 'dotenv';
+dotenv.config();
+import express from 'express';
+import mongoose from 'mongoose';
+import cors from 'cors';
+import morgan from 'morgan';
+
+const app = express();
+
+app.use(express.json({ limit: '30mb' }));
+app.use(express.urlencoded({ limit: '30mb', extended: true }));
+app.use(cors());
+app.use(morgan('dev'));
+
+app.get('/', (req, res) => {
+  res.send('Hello to the bug tracker API');
+});
+
+const PORT = process.env.PORT || 8080;
+const CONNECTION_URI = process.env.MONGO_URI || '';
+
+mongoose.set('strictQuery', false);
+mongoose.connect(CONNECTION_URI)
+  .then(() => {
+    app.listen(PORT, () => {
+      console.log(`Server running on ${PORT}`);
+    })
+  }).catch((err) => {
+    console.log(`Connection error: ${err}`);
+  });
+
