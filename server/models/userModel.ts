88a0e5340329e231049e9f9f3ec0b0e8bd4b042b
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-01-03 22:14:12 +0100

    Add authent

--- server/models/userModel.ts
+++ server/models/userModel.ts
@@ -2,10 +2,10 @@ import mongoose, { InferSchemaType } from 'mongoose';
 
 const UserSchema = new mongoose.Schema(
   {
-    name: { type: String, required: true },
-    image: { type: String, required: true },
+    name: { type: String, required: true, min: 2, max: 50 },
+    image: { type: String, required: true, default: '' },
     email: { type: String, unique: true, required: true },
-    password: { type: String, required: true },
+    password: { type: String, required: true, min: 6 },
     googleId: { type: String, required: false },
     admin: { type: Boolean, default: false },
   },
