14556b3843c1fd6e854e9a7a17787f046d11549a
Charles <okoyecharles509@gmail.com>
2023-01-03 15:18:34 +0100

    Create user controlller

---
+++ server/controller/userController.ts
@@ -0,0 +1,60 @@
+import bcrypt from 'bcryptjs';
+import jwt from 'jsonwebtoken';
+import User from '../models/userModel';
+
+const secret = process.env.JWT_SECRET || '';
+const tokenExpiration = process.env.NODE_ENV === 'development' ? '1d' : '1hr';
+
+export const register = async (req: any, res: any) => {
+  const { name, image, email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+    if (userExists) {
+      return res.status(400).json({ message: 'User already exists' });
+    };
+
+    const salt = await bcrypt.genSalt(12);
+    const hashedPassword = await bcrypt.hash(password, salt);
+
+    const user = await User.create({
+      name,
+      email,
+      password: hashedPassword,
+      image
+    });
+
+    res.status(200).json({ user, token: generateToken(user._id) });
+
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong... Please try again' });
+  }
+};
+
+export const login = async (req: any, res: any) => {
+  const { email, password } = req.body;
+
+  try {
+    const userExists = await User.findOne({ email });
+
+    if (!userExists) {
+      return res.status(404).json({ message: 'User does not exist' });
+    }
+
+    const isPasswordCorrect = await bcrypt.compare(password, userExists.password);
+    if (!isPasswordCorrect) {
+      return res.status(400).json({ message: 'Invalid credentials' });
+    }
+
+    res.status(200).json({ user: userExists, token: generateToken(userExists._id) });
+  } catch (error) {
+    res.status(500).json({ message: 'Something went wrong... Please try again' });
+  }
+}
+
+const generateToken = (id: any) => {
+  const token = jwt.sign({ id }, secret, {
+    expiresIn: tokenExpiration
+  });
+  return token;
+}
