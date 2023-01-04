bd7ee8c131dfd0d7383a9cad04e72fbf7d6d8bca
Nicholas Emmanuel <nicholasemmanuel321@gmail.com>
2023-01-04 16:11:43 +0100

    Add tailwind config

---
+++ client/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    './pages/**/*.{js,ts,jsx,tsx}',
+    './components/**/*.{js,ts,jsx,tsx}',
+  ],
+  theme: {
+    extend: {
+      colors: {
+        primary: '#0070f3',
+        secondary: '#ff6600',
+      },
+    },
+    plugins: [],
+  },
+};
