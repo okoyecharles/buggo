f36d53d19250827635b0eba905d7dc352d4941c1
Charles <okoyecharles509@gmail.com>
2023-01-16 08:56:01 +0100

    Optimize member panel

--- client/components/project/ProjectCard/Members.tsx
+++ client/components/project/ProjectCard/Members.tsx
@@ -20,17 +20,26 @@ const ProjectCardMembers: React.FC<ProjectCardMembersProps> = ({
   return (
     <div className="flex mb-2 gap-2 lg:items-center text-ss lg:h-7">
       <h4 className="font-bold text-gray-400 flex items-center">MEMBERS:</h4>
-      <div className="flex gap-2 items-center h-7">
+      <div className="flex gap-2 items-center h-7 overflow-hidden">
         {project.team.length ? (
-          project.team.map((member) => (
-            <Image
-              src={member.image}
-              alt={member.name}
-              width={28}
-              height={28}
-              className="h-full object-center object-cover rounded-full ring-1 ring-orange-500/50 bg-gray-800"
-            />
-          ))
+          <>
+            {project.team.slice(0, 3).map((member) => (
+              <Image
+                src={member.image}
+                alt={member.name}
+                width={28}
+                height={28}
+                className="h-full object-center object-cover rounded-full ring-1 ring-orange-500/50 bg-gray-800"
+              />
+            ))}
+            {project.team.length > 3 ? (
+              <p className="text-sm font-noto font-semibold text-orange-500/90 whitespace-nowrap">
+                +{project.team.length - 3} more
+              </p>
+            ) : (
+              ""
+            )}
+          </>
         ) : (
           <p className="text-ss text-gray-500">No team members</p>
         )}
